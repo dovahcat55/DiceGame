bceb9f869bbe8551effa8f82f6a771a5889c5a85
Dovahkiin <dovahkiin@dovahkiins-air.rad.liberty.edu>
2024-08-17 10:46:17 -0700

    Initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Who.What = Value
+        //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        //diceImageView1.alpha = 0.5 //change opacitys
+        //diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("The Button Worked.") //To be printed in the debug terminal
+        //Who.What = Value
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+    }
 }
 
